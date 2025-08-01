538cec3a3955b463be7f2ea9e2e7515add1cf585
incognito <sylsterior@gmail.com>
2025-08-01 17:25:44 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+attendance=int(input("How many days did you ateednd classes"))
+if attendance>=75:
+ print("You are allowed to take the exams")
+else:
+ Medicalcauses=input("were you sick? :")
+ if Medicalcauses==("Yes" or "yes"or "YES"or"yea" or "Yea" or"YEA"):
+  print("You can take exams ")
+ else:
+  print("You are not allowed to take the exams")
